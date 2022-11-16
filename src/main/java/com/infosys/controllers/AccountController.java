8c6b14d1736c41fb20f7819c7b7035e54b6bf32f
thisonewillworkforsure <ben6228@yahoo.com>
2022-11-16 13:56:58 -0500

    backend finished

--- src/main/java/com/infosys/controllers/AccountController.java
+++ src/main/java/com/infosys/controllers/AccountController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +23,23 @@ public class AccountController {
 	@Autowired
 	private AccountService accountService;
 	
-	@GetMapping
+	@GetMapping("/all")
 	public ResponseEntity<List<Account>> getAllAccounts(){
-		System.out.println("WHOAAAAA");
 		return ResponseEntity.ok(accountService.getAllAccounts());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Account> createAccount(@RequestBody Account account){
+		return ResponseEntity.ok(accountService.createAccount(account));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Account> updateAccount(@RequestBody Account account){
+		return ResponseEntity.ok(accountService.updateAccount(account));
+	}
+	
+	@GetMapping
+	public ResponseEntity<Account> getAccount(@RequestBody Account account){
+		return ResponseEntity.ok(accountService.getAccount(account));
 	}
 }
